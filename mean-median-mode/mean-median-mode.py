271b6dacb76070321686793cf6e29b3035b33b40
Suhas <95905505+Suhas7842@users.noreply.github.com>
2026-09-15 19:27:28 +0530

    Solve mean-median-mode

---
+++ mean-median-mode/mean-median-mode.py
@@ -0,0 +1,13 @@
+from collections import Counter
+import numpy as np
+
+def mean_median_mode(x: list) -> dict:
+    """
+    Returns a dictionary with mean, median, and mode.
+    """
+    # Write code here
+    return {
+        "mean": float(np.mean(x)),
+        "median": float(np.median(x)),
+        "mode": float(Counter(x).most_common(1)[0][0])
+    }
