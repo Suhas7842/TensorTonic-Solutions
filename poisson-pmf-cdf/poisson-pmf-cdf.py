20d3fd6c5f6ac0e54af18b0d5480e180bac58951
Suhas <95905505+Suhas7842@users.noreply.github.com>
2026-09-18 21:38:59 +0530

    Solve poisson-pmf-cdf

---
+++ poisson-pmf-cdf/poisson-pmf-cdf.py
@@ -0,0 +1,18 @@
+import math
+
+def poisson_pmf_cdf(lam: float, k: int) -> dict:
+    """
+    Returns a dictionary with pmf and cdf.
+    """
+    # Write code here
+    pmf = math.exp(-lam) * (lam ** k) / math.factorial(k)
+
+    cdf = sum(
+        math.exp(-lam) * (lam ** i) / math.factorial(i)
+        for i in range(k + 1)
+    )
+
+    return {
+        "pmf": pmf,
+        "cdf": cdf
+    }
