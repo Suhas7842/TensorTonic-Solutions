96d4ea1d7abec434b6e2ba11831972e1d4eff995
Suhas <95905505+Suhas7842@users.noreply.github.com>
2026-09-21 20:47:30 +0530

    Solve nesterov-momentum

---
+++ nesterov-momentum/nesterov-momentum.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def nesterov_momentum_step(w: list, v: list, grad: list, lr: float = 0.01, momentum: float = 0.9) -> dict:
+    """
+    Returns a dictionary with new_w and new_v.
+    """
+    # Write code here
+    w = np.array(w, dtype=float)
+    v = np.array(v, dtype=float)
+    grad = np.array(grad, dtype=float)
+
+    new_v = momentum * v + lr * grad
+    new_w = w - new_v
+
+    return {
+        "new_w": new_w,
+        "new_v": new_v
+    }
